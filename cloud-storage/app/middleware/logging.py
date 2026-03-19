651211e19d06ef135bdd8a851aeabf5021d96af2
Захар <zahar.gaskov@ya.ru>
2026-03-20 04:35:44 +0700

    logging.py

--- cloud-storage/app/middleware/logging.py
+++ cloud-storage/app/middleware/logging.py
@@ -12,7 +12,6 @@ def configure_logging():
     structlog.configure(
         processors=[
             structlog.stdlib.add_log_level,
-            structlog.stdlib.add_logger_name,
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.JSONRenderer(),
         ],
